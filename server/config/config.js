8c035e4065bf157053ff63e968eaf120d0c1c4f5
sagarluitel01 <sagar_luitel25@yahoo.com>
2020-03-01 19:54:47 -0700

    config mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://<sagar01>:<D3goldhap>@ds045897.mlab.com:45897/cas-auction',
+    database: 'mongodb://<sagar1>:<Denver1>@ds045897.mlab.com:45897/cas-auction',
     port: process.env.PORT || 8080
 }
